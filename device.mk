08075b6bbc3e9b2ba4308dbe69f7060d9d1899d4
Rudy <paleloebarbershop@gmail.com>
2025-12-24 19:07:21 +0700

    marble: Build wtth private keys
    Enable inclusion of private keys by uncommenting the line.
    
    Signed-off-by: Rudy <paleloebarbershop@gmail.com>

--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ TARGET_NFC_SUPPORTED_SKUS := marble
 # Inherit from xiaomi sm8450-common
 $(call inherit-product, device/xiaomi/sm8450-common/common.mk)
 
+# Enable private keys
+-include vendor/private/keys/keys.mk
+
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/marble/marble-vendor.mk)
 
